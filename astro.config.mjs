aea66ff60e080ef3929224a57d97439f48dc2747
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2026-04-23 21:10:16 +0100

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mcsc-holding.com',
+  site: 'https://mcsc-group.com',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr', 'it'],
